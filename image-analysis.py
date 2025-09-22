d63fff6bba0fde917a68c3caefe34aab3f2f7873
AngJianming <angjianming2005@hotmail.com>
2025-09-22 16:19:45 +0800

    feat: added python files haven't write the code for testing yet..

---
+++ image-analysis.py
@@ -0,0 +1,15 @@
+import argparse
+
+import vertexai 
+from vertexai.preview.vision_model import ImageGenerationModel
+
+def generate_image () -> vertexai.preview.vision_models.ImageGenerationResponses:
+  """_summary_
+
+  Returns:
+    e.g: nothing yet.. relax
+      vertexai.preview.vision_models.ImageGenerationResponses: _description_
+  """
+
+
+
